dc6d6ba5c3ab778de303bb20e1c5e4a0c666e81c
RenatoLlerenaa <roosevelt.llerenaa@ug.edu.ec>
2016-02-29 11:16:44 -0500

    Añadir MAP-REDUCE
    ALgoritmo kmeans con map reduce

---
+++ SOURCE MAP-REDUCE/MAPPER.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Use the sys module
+import sys
+#file=open('datos.txt','r')
+# 'file' in this case is STDIN
+def read_input(file):
+    # Split each line into words
+    for line in file:
+        yield line.split()
+
+def main(separator='\t'):
+    
+    # Read the data using read_input
+    data = read_input(sys.stdin)
+    # Process each words returned from read_input
+    for words in data:
+        # Process each word
+        for word in words:
+            # Write to STDOUT
+            print '%s%s%d' % (word, separator, 1)
+def leertxt():
+    archi=open('datos.txt','r')
+    linea=archi.readline()
+    while linea!="":
+        print linea
+        linea=archi.readline()
+    archi.close()
+if __name__ == "__main__":
+    main()
+    
